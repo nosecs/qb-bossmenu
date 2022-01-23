8a1ab4a362c0ccfd8f1ca7eede0223205b397a00
nosecs <95661721+nosecs@users.noreply.github.com>
2022-01-23 16:29:38 +0100

    Merge With Old System

--- config.lua
+++ config.lua
@@ -1,10 +1,10 @@
 Config = {}
 
 Config.Jobs = {
-    ['police'] = vector3(448.4, -973.2, 30.6),
-    ['ambulance'] = vector3(310.1, -599.43, 43.29),
+    ['police'] = vector3(460.12, -985.4, 30.73),
+    ['ambulance'] = vector3(334.88, -592.56, 43.28),
     ['realestate'] = vector3(-124.786, -641.486, 168.820),
     ['taxi'] = vector3(903.32, -170.55, 74.0),
-    ['cardealer'] = vector3(-32.0, -1114.2, 26.4),
+    ['cardealer'] = vector3(-24.48, -1101.95, 27.27),
     ['mechanic'] = vector3(-347.41, -133.48, 39.01)
 }
